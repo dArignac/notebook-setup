205d8b7ec58fc8121a821873a175e653838d2f8c
Alexander Herrmann <darignac@gmail.com>
2016-02-13 13:27:11 +0100

    added first two setup steps

---
+++ setup.0.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "To apply sudo, become root, install sudo and execute 'adduser USERNAME sudo', then log out and in."
+echo "Afterwards continue with setup.1.sh"
---
+++ setup.1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enabling contrib nonfree in apt":
+sudo sed -i 's/main/main contrib non-free/' sources.list
+[ -f /etc/apt/sources.list.d/deb-multimedia.list ] || echo "deb ftp://ftp.deb-multimedia.org stable main non-free" | sudo dd of=/etc/apt/sources.list.d/deb-multimedia.list
+sudo aptitude update
+sudo apt-get install deb-multimedia-keyring
+sudo aptitude install firmware-iwlwifi
+echo "To get the wifi running, please restart and continue with setup.2.sh!"
