cbfd571c0821e19c15010ca8626505b6405cb518
Alexander Herrmann <darignac@gmail.com>
2016-03-04 19:37:19 +0100

    added bumblebee

--- setup.1.sh
+++ setup.1.sh
@@ -4,5 +4,5 @@ sudo sed -i 's/main/main contrib non-free/' sources.list
 [ -f /etc/apt/sources.list.d/deb-multimedia.list ] || echo "deb ftp://ftp.deb-multimedia.org stable main non-free" | sudo dd of=/etc/apt/sources.list.d/deb-multimedia.list
 sudo aptitude update
 sudo apt-get install deb-multimedia-keyring
-sudo aptitude install firmware-iwlwifi firmware-realtek
+sudo aptitude install firmware-iwlwifi firmware-realtek bumblebee-nvidia primus
 echo "To get the wifi running, please restart and continue with setup.2.sh!"
