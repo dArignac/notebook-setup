0cda44d6c564820035858f7bc4f9f207f02affbf
Alexander Herrmann <darignac@gmail.com>
2016-05-22 11:22:48 +0200

    added vobvopy

--- setup.2.sh
+++ setup.2.sh
@@ -2,7 +2,7 @@
 DIRHERE=`pwd`
 
 echo "Install basic packages:"
-sudo aptitude install vim curl keepass2 encfs git htop multitail p7zip-rar clamav vlc poedit s3cmd libdvdread4
+sudo aptitude install vim curl keepass2 encfs git htop multitail p7zip-rar clamav vlc poedit s3cmd libdvdread4 vobcopy
 sudo update-alternatives --config editor
 
 # source: https://wiki.debian.org/BluetoothUser/a2dp
