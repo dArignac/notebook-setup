6b9af8bd83feafba8f9bcbb0b88c38f946ae64ea
Alexander Herrmann <darignac@gmail.com>
2016-06-25 17:15:05 +0200

    added imagemagick and jpegoptim

--- setup.2.sh
+++ setup.2.sh
@@ -2,7 +2,7 @@
 DIRHERE=`pwd`
 
 echo "Install basic packages:"
-sudo aptitude install vim curl keepass2 encfs git htop multitail p7zip-rar clamav vlc poedit s3cmd libdvdread4 vobcopy libdvdcss2
+sudo aptitude install vim curl keepass2 encfs git htop multitail p7zip-rar clamav vlc poedit s3cmd libdvdread4 vobcopy libdvdcss2 imagemagick jpegoptim
 sudo update-alternatives --config editor
 
 # source: https://wiki.debian.org/BluetoothUser/a2dp
