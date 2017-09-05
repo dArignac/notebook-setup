a7dfc7af1ea1458c68f93abf1bd0d549e045f59c
Alexander Herrmann <darignac@gmail.com>
2017-09-05 17:49:30 +0200

    fixed wrong path, updated multimedia keyring installation and added graphics driver hint to help text

--- setup.1.sh
+++ setup.1.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Enabling contrib nonfree in apt":
-sudo sed -i 's/main/main contrib non-free/' sources.list
+sudo sed -i 's/main/main contrib non-free/' /etc/apt/sources.list
 [ -f /etc/apt/sources.list.d/deb-multimedia.list ] || echo "deb ftp://ftp.deb-multimedia.org stable main non-free" | sudo dd of=/etc/apt/sources.list.d/deb-multimedia.list
-sudo aptitude update
-sudo apt-get install deb-multimedia-keyring
+echo "Ignore the key warning for multimedia-keyring:"
+sudo apt-get update -oAcquire::AllowInsecureRepositories=true
+sudo apt-get install deb-multimedia-keyring -oAcquire::AllowInsecureRepositories=true
+echo "now back to normal:"
+sudo apt-get install aptitude
 sudo aptitude install firmware-iwlwifi firmware-realtek bumblebee-nvidia primus
-echo "To get the wifi running, please restart and continue with setup.2.sh!"
+echo "To get the wifi and NVIDIA driver running, please restart and continue with setup.2.sh!"
