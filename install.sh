61bc0c5e7b1ab35071b35be0d22b3cda635621b5
Alexander Herrmann <darignac@gmail.com>
2018-12-01 14:32:49 +0100

    added hidrive

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 echo "Installing all packages:"
 
 declare -a INSTALLER=(
+"hidrive"
 "puddletag"
 )
 
---
+++ scripts/hidrive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Preparing for HiDrive via WebDAV:"
+sudo aptitude install davfs2 ca-certificates
+sudo usermod -aG davfs2 $USER
+echo "# HiDrive" | sudo dd of=/etc/fstab
+echo "https://webdav.hidrive.strato.com/ /mnt/hidrive davfs noauto,user,rw 0 0" | sudo dd of=/etc/fstab
+sudo mkdir -p /mnt/hidrive
+echo "Done! Mount it via 'mount /mnt/hidrive'"
