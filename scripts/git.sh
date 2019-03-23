368e64aef96ee54eb93d8a598c3eb35e5352184d
Alexander Herrmann <darignac@gmail.com>
2019-03-23 15:42:43 +0100

    added git-lfs

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,3 +6,8 @@ git config --global user.email "darignac@gmail.com"
 git config --global credential.helper cache
 # cache 4 hours
 git config --global credential.helper 'cache --timeout=14400'
+
+echo "adding git-lfs"
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo aptitude update && sudo aptitude install git-lfs
+git lfs install
