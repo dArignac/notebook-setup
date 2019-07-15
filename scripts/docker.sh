21fbe5cab7f6a6184a1f6efe42e38f47b80471e4
Alexander Herrmann <darignac@gmail.com>
2019-07-15 17:10:56 +0200

    fixed echo

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # see https://docs.docker.com/install/linux/docker-ce/ubuntu/
-echo" setting up docker..."
+echo "setting up docker..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
