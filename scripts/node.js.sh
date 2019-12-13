ea13e29e73d2709b63ef1fbf38ab7a426e409856
Alexander Herrmann <darignac@gmail.com>
2019-12-13 13:42:31 +0100

    updated nvm

--- scripts/node.js.sh
+++ scripts/node.js.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installting nvm"
 mkdir -p $HOME/.nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | PROFILE=~/.zshrc bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | PROFILE=~/.zshrc bash
 
 echo "Installing yarn"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
