95966cc66c4dcc2c3f0089c085f10ca4995d76ce
Alexander Herrmann <darignac@gmail.com>
2019-12-27 15:36:23 +0100

    added clang for flutter

--- scripts/android_dev.sh
+++ scripts/android_dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Preparing for Android Development Tooling"
+# for android emulation, speeds up the emulator
 sudo aptitude install qemu-kvm
 sudo adduser $USER kvm
+
+# flutter linux desktop requirements
+sudo aptitude install clang
+
 # FIXME add the android studio setup stuff
