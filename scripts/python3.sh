6e0829b445d6f3d97a61e9dd725a5d2ee122173b
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2020-07-23 20:19:06 +0200

    adjusted python3 installation and just added pipenv

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "Installing Python:"
-sudo aptitude install python3-distutils
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-python3 /tmp/get-pip.py --user
+echo "Installing pipenv:"
+sudo aptitude install pipenv
