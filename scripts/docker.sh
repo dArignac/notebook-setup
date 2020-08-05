0453a25adfabc786b03c702f9728896f09fdb631
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2020-08-05 18:20:14 +0200

    updated docker compose version

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,7 +6,7 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo aptitude update
 sudo aptitude install docker-ce docker-ce-cli containerd.io
-sudo -E curl -L -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m`
+sudo -E curl -L -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m`
 sudo chmod +x /usr/local/bin/docker-compose
 sudo -E curl -L -o /etc/bash_completion.d/docker-compose https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose
 sudo usermod -aG docker $USER
