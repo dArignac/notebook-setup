d4c62f8bdb84349559c6608c7a8b631f7428b43e
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-01-08 10:36:37 +0100

    Update node.js.sh

--- scripts/node.js.sh
+++ scripts/node.js.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "Installting nvm"
 mkdir -p $HOME/.nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | PROFILE=~/.zshrc bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | PROFILE=~/.zshrc bash
 
 echo "Installing yarn"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
 sudo apt install yarn
-
