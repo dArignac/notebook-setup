abb10b6d20ebce89dafff2b81c8bec5eb64957ab
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-08-01 07:36:56 +0200

    use volta for nodejs

--- scripts/node.js.sh
+++ scripts/node.js.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-echo "Installing nvm"
-mkdir -p $HOME/.nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | PROFILE=~/.zshrc bash
-
-echo "Installing yarn"
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update
-sudo apt install yarn
+curl https://get.volta.sh | bash
