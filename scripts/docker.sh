27a037418a9e9df35f60969694d391f9f7dbe957
Alexander <106409+dArignac@users.noreply.github.com>
2021-08-23 14:58:43 +0200

    adjusted docker

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,9 +4,9 @@ echo "setting up docker..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo aptitude update
-sudo aptitude install docker-ce docker-ce-cli containerd.io
-sudo -E curl -L -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m`
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo -E curl -L -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m`
 sudo chmod +x /usr/local/bin/docker-compose
 sudo -E curl -L -o /etc/bash_completion.d/docker-compose https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose
 sudo usermod -aG docker $USER
