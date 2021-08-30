0c76a2205b5fd2439d860a3afbf72c79fa2ea557
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-08-30 10:23:59 +0200

    removed email

--- scripts/git.sh
+++ scripts/git.sh
@@ -2,7 +2,6 @@
 echo "setting up git..."
 sudo apt install git
 git config --global user.name "Alexander Herrmann"
-git config --global user.email "106409+dArignac@users.noreply.github.com"
 git config --global credential.helper cache
 # cache 4 hours
 git config --global credential.helper 'cache --timeout=14400'
