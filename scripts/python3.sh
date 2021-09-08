73e93dd8ccadbcb2383d8e8527473fa76ab47ce6
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-09-08 10:52:15 +0200

    added python3-venv

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Installing pipenv:"
-sudo apt install pipenv
+echo "Installing Python:"
+# python3-venv is for Visual Studio Code Azure Functions extension
+sudo apt install pipenv python3-venv
