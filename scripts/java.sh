703a67f16266769e1f8ce6ffc33e4c7fc22973a9
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-09-08 10:46:48 +0200

    updated java version

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "setting up Java..."
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
-jabba install openjdk@1.12.0
+jabba install openjdk@1.16.0
