9dc5604ab6a062688483dbe2e2402d150e31930f
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-11-04 14:47:41 +0100

    added elements

---
+++ scripts/element.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Installing Element..."
+sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
+sudo apt update
+sudo apt install element-desktop
