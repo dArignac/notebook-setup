7841199fecd3a322701c8038fdea9f09f18ae510
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2023-08-01 21:01:30 +0200

    change to pdm

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "Installing Python:"
-# python3-venv is for Visual Studio Code Azure Functions extension
-sudo apt install pipenv python3-venv
+# install pdm
+curl -sSL https://pdm.fming.dev/dev/install-pdm.py | python3 -
+
+mkdir -p $ZSH_CUSTOM/plugins/pdm
+pdm completion zsh > $ZSH_CUSTOM/plugins/pdm/_pdm
+
