95baab5c89223835c29814f79ed9698a421a4e4c
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2024-08-22 14:58:14 +0200

    removed Bitwarden

--- scripts/bitwarden.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "setting up Bitwarden..."
-curl -L -o ~/Bitwarden.AppImage https://vault.bitwarden.com/download/?app=desktop&platform=linux
-cp .local/share/applications/BitWarden.desktop ~/.local/share/applications/BitWarden.desktop
