49b186241da553d07aeb6ba625598b8c25b4ab36
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-01-17 12:30:44 +0100

    fix insync

--- scripts/insync.sh
+++ scripts/insync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Installing Insync client (with repo):"
+echo "Installing Insync client:"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-[ -f /etc/apt/sources.list.d/insync.list ] || echo "deb http://apt.insync.io/ubuntu eoan non-free contrib" | sudo dd of=/etc/apt/sources.list.d/insync.list
+[ -f /etc/apt/sources.list.d/insync.list ] || echo "deb http://apt.insync.io/ubuntu  $(. /etc/os-release && echo "$VERSION_CODENAME") non-free contrib" | sudo dd of=/etc/apt/sources.list.d/insync.list
 sudo apt update
 sudo apt install insync
