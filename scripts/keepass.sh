1adf36bca9f10245b1c057e600141a601d0ebbe8
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-01-17 12:31:56 +0100

    cleanup

--- scripts/keepass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed -i '/<\/PasswordGenerator>/{ 
-   r ../.config/KeePass/keepass.profiles.xml
-   a \</PasswordGenerator>
-   d 
-}' ~/.config/KeePass/KeePass.config.xml
--- scripts/kid3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo add-apt-repository ppa:ufleisch/kid3
-sudo apt-get update
-sudo apt-get install kid3-qt 
-echo "Done!"
